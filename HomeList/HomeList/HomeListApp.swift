9ce001969a267d3fbbba73998f744881ff718187
David Jangdal <djangdal@gmail.com>
2023-04-20 19:46:32 +0200

    Empty project

---
+++ HomeList/HomeList/HomeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeListApp.swift
+//  HomeList
+//
+//  Created by David Jangdal on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct HomeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
