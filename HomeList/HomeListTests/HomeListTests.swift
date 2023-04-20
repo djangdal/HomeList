8560b0e1094e20abf98df8a83ac2f5483e8202dc
David Jangdal <djangdal@gmail.com>
2023-04-20 23:39:05 +0200

    Added test for detailed property

--- HomeList/HomeListTests/HomeListTests.swift
+++ HomeList/HomeListTests/HomeListTests.swift
@@ -58,4 +58,29 @@ final class HomeListTests: XCTestCase {
         XCTAssertEqual(area.averagePrice, 2)
         XCTAssertEqual(area.imageURL, URL(string: "http://example.com")!)
     }
+
+    func testCreateDetailedProperty_hasCorrectValues() throws{
+        let response = DetailedResponse(type: "Value 1",
+                                        id: "Value 2",
+                                        askingPrice: 1,
+                                        municipality: "Value 4",
+                                        area: "Value 3",
+                                        daysSincePublish: 4,
+                                        livingArea: 5,
+                                        numberOfRooms: 6,
+                                        streetAddress: "Value 6",
+                                        image: URL(string: "http://example.com")!,
+                                        description: "Value 7",
+                                        patio: "Value 8")
+        let detailed = DetailedProperty(apiResponse: response)
+        XCTAssertEqual(detailed.id, "Value 2")
+        XCTAssertEqual(detailed.imageURL, URL(string: "http://example.com")!)
+        XCTAssertEqual(detailed.address, "Value 6")
+        XCTAssertEqual(detailed.municipality, "Value 4")
+        XCTAssertEqual(detailed.price, 1)
+        XCTAssertEqual(detailed.area, "Value 3")
+        XCTAssertEqual(detailed.numberOfRooms, 6)
+        XCTAssertEqual(detailed.livingArea, 5)
+        XCTAssertEqual(detailed.description, "Value 7")
+        XCTAssertEqual(detailed.patio, "Value 8")    }
 }
